3a75b3a1448462e8b3de379a02107a078212b273
sabir222 <mrsabir4@gmail.com>
2024-12-17 21:42:06 +0100

    update : setting up cmake

---
+++ build/CMakeFiles/parser.dir/src/utils.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/parser.dir/src/utils.c.o: \
+ /home/sabir/Projects/transpiler_in_c/src/utils.c \
+ /usr/include/stdc-predef.h \
+ /home/sabir/Projects/transpiler_in_c/include/utils.h
---
+++ include/utils.h
@@ -0,0 +1,8 @@
+// include/utils.h
+#ifndef UTILS_H  // Prevents multiple inclusions
+#define UTILS_H
+
+// Function declaration: add two numbers
+int add(int a, int b);
+
+#endif // UTILS_H
